c87369d7912d3ed4d9763b7bc6c0ae02adeb94d5
Sven Fischer <git-dev@linux4tw.de>
2021-11-03 01:15:25 +0100

    fail on error, but not when xmllint returns no result

--- stralsund.sh
+++ stralsund.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# dependencies: wget xmlstarlet
+# dependencies: wget xmllint
+set -eux
 
 town="stralsund"
 
@@ -18,7 +19,11 @@ do
   echo $link
   wget -O $i.xml $link
   # select the <text> content from the the xml | get lines starting with * | remove the * and whitespaces, remove opening link brackets until the pipe, remove opening link brackets, remove closing link markdown, remove parenthesis
-  xmllint --xpath "//*[local-name()='text']/text()" $i.xml | grep ^\* | sed 's/\*\s*//g ; s/\[\[.*|//g ; s/\[\[//g ; s/\]\].*$//g ; s/([^)]*)//g' | grep -v "^''" > $i.txt
+  content=$(xmllint --xpath "//*[local-name()='text']/text()" $i.xml || true)
+  if [ -n "$content" ]
+  then
+    echo "$content" | grep ^\* | sed 's/\*\s*//g ; s/\[\[.*|//g ; s/\[\[//g ; s/\]\].*$//g ; s/([^)]*)//g' | grep -v "^''" > $i.txt
+  fi
 done
 
 find -name '*.txt' -print0 | xargs -r0 sed -e 's/[[:blank:]]\+$// ; s/^[[:blank:]]\+//' -i
